8ba93606b86596611a7b5fb3d40ab5aaf463e1ac
Artyom Trityak <art.trityak@gmail.com>
2013-11-05 09:26:19 -0800

    Pages

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function initialize() {
+    var myLatlng = new google.maps.LatLng(46.433103,30.761803);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Гагаринское плато 5'
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+$(document).ready(function(){
+    $('nav a').click(function () {
+        var elementHref = $(this).attr('href');
+        var destination = $(elementHref).offset().top - 60;
+        $('html, body').animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+    });
+});
